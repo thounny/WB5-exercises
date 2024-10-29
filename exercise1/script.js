fca379c4cad3e181f7b40b8f460cf14a644167a2
thounny <thounnykeo@gmail.com>
2024-10-29 13:21:11 -0400

    Exercise 1 completed

--- exercise1/script.js
+++ exercise1/script.js
@@ -6,4 +6,10 @@ window.onload = function () {
   for (var i = 0; i < imageGroupElements.length; i++) {
     imageGroupElements[i].style.border = "3px solid red";
   }
+
+  // Find all images without an alt attribute and assign them the text "graffiti image"
+  var imagesWithoutAlt = document.querySelectorAll("img:not([alt])");
+  for (var j = 0; j < imagesWithoutAlt.length; j++) {
+    imagesWithoutAlt[j].setAttribute("alt", "graffiti image");
+  }
 };
